a472a7ac60c54d5e5354cf287673008e15d74661
omidamin68 <102811843+omidamin68@users.noreply.github.com>
2024-03-02 06:32:35 +0330

    Updates

--- assets/swipe-back-legacy-B-pF7wCt.js
+++ assets/swipe-back-legacy-B-pF7wCt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dt4U-f6Z.js","./index3-legacy-DO2LPL6a.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-B0IpJC7j.js","./index3-legacy-B1qdYRKT.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
